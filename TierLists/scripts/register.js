fbdeeb8183cd1a5df0023bcb359067d25a6ccc93
Klounadich <kekers507y@gmail.com>
2025-06-30 09:08:28 +0300

    update js

--- TierLists/scripts/register.js
+++ TierLists/scripts/register.js
@@ -1,27 +1,35 @@
-document.getElementById("registerForm").addEventListener("submit",async(e)=>{
+document.getElementById("registerForm").addEventListener("submit", async (e) => {
     e.preventDefault();
+    
 
-    const data= {
+    const data = {
         username: document.getElementById("username").value,
         email: document.getElementById("email").value,
         password: document.getElementById("password").value
-
     };
+    
 
-    try{
-        const responce= await fetch("hhtp://192.168.0.101/api/Auth/Register",{
+    try {
+        
+        const response = await fetch("http://192.168.0.101:5000/api/auth/register", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify.json(),
+            body: JSON.stringify(data),
+                
         });
-        const result= await responce.json();
         
+        
+        const result = await response.json();
+        
+
+        if (response.ok) {
+            
+        } else {
+            alert("Ошибка: " + (result.message || "Unknown error"));
+        }
+    } catch (error) {
         
     }
-
-    catch(error){
-            console.error(error);
-        }
 });
